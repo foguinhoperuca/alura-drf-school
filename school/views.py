3cc62ffe6bc162bf03e0a96a1e645f8562f24e1a
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-04-24 10:28:20 -0300

    feat: API com Django 3: Versionamento, Cabeçalhos e CORS.
    Course finished.

--- school/views.py
+++ school/views.py
@@ -1,10 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListAPIView
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.viewsets import ModelViewSet
 
-from school.permissions import StrictDjangoModelPermissions
 from school.models import Student, Course, Enrollment
+from school.permissions import StrictDjangoModelPermissions
 from school.serializer import StudentSerializer, StudentSerializerV2, CourseSerializer, EnrollmentSerializer, ListEnrollmentsStudentsSerializer, ListStudentsEnrollmentsSerializer
 
 
@@ -31,6 +33,15 @@ class EnrollmentViewSet(ModelViewSet):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
 
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=HTTP_201_CREATED)
+            response['Location'] = request.build_absolute_uri() + str(serializer.data['id'])
+
+            return response
+
 
 class ListEnrollmentsStudents(ListAPIView):
     serializer_class = ListEnrollmentsStudentsSerializer
@@ -44,4 +55,3 @@ class ListStudentsEnrollments(ListAPIView):
 
     def get_queryset(self):
         return Enrollment.objects.filter(course_id=self.kwargs['pk'])
-
--- setup/settings.py
+++ setup/settings.py
@@ -50,12 +50,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'school'
+    'school',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -140,13 +142,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated',
-                                   'rest_framework.permissions.DjangoModelPermissions'],
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication'],
+    # 'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', 'rest_framework.permissions.DjangoModelPermissions'],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication'],
     'DEFAULT_THROTTLE_CLASSES': ['rest_framework.throttling.AnonRateThrottle'],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '5/day'
+        'anon': '1000/day'
     }
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
